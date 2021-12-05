df1c00a7c8760d6b206cc4c6eff9a98ee60406e6
Sara Bensarghin <sbensarg@e1r9p1.1337.ma>
2021-12-05 21:19:21 +0100

    fixing problem of s

--- philo.c
+++ philo.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chicky <chicky@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sbensarg <sbensarg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 19:42:29 by sbensarg          #+#    #+#             */
-/*   Updated: 2021/12/04 21:05:44 by chicky           ###   ########.fr       */
+/*   Updated: 2021/12/05 21:16:20 by sbensarg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long current_timestamp()
+long long	current_timestamp(void)
 {
 	struct		timeval te; 
 	long long	milliseconds;
@@ -52,86 +52,120 @@ t_philo	*init_data(int argc, char **argv)
 		}
 		pthread_mutex_init(&data->print, NULL);
 		data->count_eat = 0;
+		data->last_meal = 0;
 		pthread_mutex_init(&data->num_of_meals, NULL);
+		data->count_eat = malloc (sizeof(int) * data->nbr_of_philos);
 	}
 	return (data);
 }
 
-void ft_print_state(t_philo *philo, char *str)
+void	ft_print_state(t_local_data *ldata, t_philo *philo, char *str)
 {
-    long long cur_time;
-    cur_time = current_timestamp();
-    pthread_mutex_lock(&philo->print);
-    printf("%lld ms philosopher %d %s\n", (cur_time - philo->prog_start), philo->idofphilo + 1, str);
-    pthread_mutex_unlock(&philo->print);
+	long long	cur_time;
+
+	cur_time = current_timestamp();
+	pthread_mutex_lock(&philo->print);
+	printf("%lld ms philosopher %d %s\n", (cur_time - philo->prog_start), ldata->id + 1, str);
+	pthread_mutex_unlock(&philo->print);
 }
 
-void ft_pickup(t_philo *philo)
+void	ft_pickup_and_eat(t_local_data *ldata, t_philo *philo)
 {
 	int right;
 	int	left;
-	
-	left = philo->idofphilo;
-	right = (philo->idofphilo + 1) % philo->nbr_of_philos;
+	int i;
+
+	i = 0;
+	left = ldata->id;
+	right = (ldata->id + 1) % philo->nbr_of_philos;
 	pthread_mutex_lock(&philo->forks[left]);
 	pthread_mutex_lock(&philo->forks[right]);
-    ft_print_state(philo, ANSI_COLOR_YELLOW "has taken a fork"  ANSI_COLOR_RESET);
+    ft_print_state(ldata, philo, ANSI_COLOR_YELLOW "has taken a fork"  ANSI_COLOR_RESET);
+	ft_print_state(ldata, philo, ANSI_COLOR_GREEN "is eating" ANSI_COLOR_RESET);
+	pthread_mutex_lock(&philo->num_of_meals);
+	ldata->count++;
+	philo->count_eat[ldata->id] = ldata->count;
+	pthread_mutex_unlock(&philo->num_of_meals);
+	usleep(philo->time_to_eat * 1000);
+	philo->last_meal = current_timestamp();
 	pthread_mutex_unlock(&philo->forks[left]);
 	pthread_mutex_unlock(&philo->forks[right]);
-	
 }
 
-void ft_eat(t_philo *philo)
+void	ft_sleep_and_think(t_local_data *ldata, t_philo *philo)
 {
-	ft_print_state(philo, ANSI_COLOR_GREEN "is eating" ANSI_COLOR_RESET);
-	usleep(philo->time_to_eat * 1000);	
+	ft_print_state(ldata, philo, ANSI_COLOR_BLUE "is sleeping" ANSI_COLOR_RESET);
+	usleep(philo->time_to_sleep * 1000);
+	ft_print_state(ldata, philo, ANSI_COLOR_MAGENTA "is thinking" ANSI_COLOR_RESET);
 }
-
 
 void*	trythis(void *data)
 {   
 	t_philo *philo;
-	
+	t_local_data *var;
+
 	philo = (t_philo *)data;
-	
+	var = malloc(sizeof(t_local_data));
+	philo->last_meal = philo->prog_start;
+	var->id = philo->idofphilo;
+	var->count = 0;
 	while (1)
 	{
-		ft_pickup(philo);
-		ft_eat(philo);
+		usleep(100);
+		ft_pickup_and_eat(var, philo);
+		ft_sleep_and_think(var, philo);
 	}
     return NULL;
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo *philo;
+	t_philo	*philo;
 	int		i;
 	int		error;
+	int		var;
 
 	philo = init_data(argc, argv);
 	i = 0;
+	var = 0;
 	philo->prog_start = current_timestamp();
 	philo->threads = (pthread_t *)malloc (sizeof(pthread_t) * philo->nbr_of_philos);
     while (i < philo->nbr_of_philos)
 	{
 		philo->idofphilo = i;
-        error = pthread_create(&(philo->threads[i]),
-                               NULL,
-                               trythis, philo);
-        if (error != 0)
-            printf("\nthread can't be created :[%s]",
-                strerror(error));
-		usleep(2000);
-        i++;
-    }
+		error = pthread_create(&(philo->threads[i]),
+					NULL,
+						trythis, philo);
+		if (error != 0)
+		printf("\nthread can't be created :[%s]",
+		strerror(error));
+		usleep(100);
+		i++;
+	}
 	i = 0;
-    while (i < philo->nbr_of_philos)
-    {
-       pthread_join(philo->threads[i], NULL); 
-       i++; 
-    }
-	  pthread_mutex_destroy(philo->forks);
-	  pthread_mutex_destroy(&philo->num_of_meals);
-	  pthread_mutex_destroy(&philo->print);
-	  
+	while (1)
+	{
+		while(i < philo->nbr_of_philos)
+		{
+			if (philo->count_eat[i] == philo->nbr_times_philo_eat)
+				var += 1;
+			else
+				var += 0;
+			i++;
+		}
+		if (var == philo->nbr_of_philos)
+			break ;
+		i = 0;
+		var = 0;
+
+	}
+	// i = 0;
+    // while (i < philo->nbr_of_philos)
+    // {
+    //    pthread_join(philo->threads[i], NULL); 
+    //    i++; 
+    // }
+	pthread_mutex_destroy(philo->forks);
+	pthread_mutex_destroy(&philo->num_of_meals);
+	pthread_mutex_destroy(&philo->print);
 }
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chicky <chicky@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sbensarg <sbensarg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 19:42:42 by sbensarg          #+#    #+#             */
-/*   Updated: 2021/12/04 19:40:23 by chicky           ###   ########.fr       */
+/*   Updated: 2021/12/05 21:18:20 by sbensarg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@
 
 typedef struct	s_philo
 {
-    int				idofphilo;
+	int				idofphilo;
 	pthread_t		*threads;
-    int				last_meal;
+    long long		last_meal;
     long			prog_start;
 	int				nbr_of_philos;
     int				time_to_die;
@@ -41,11 +41,16 @@ typedef struct	s_philo
     int				nbr_times_philo_eat;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print;
-	int				count_eat;
+	int				*count_eat;
   	pthread_mutex_t	num_of_meals;
-	long long		cur_time;
+	pthread_mutex_t	check;
 }					t_philo;
 
+typedef struct	s_local_data
+{
+    int				id;
+	int				count;
+}					t_local_data;
 
 int	ft_atoi(const char *str);
 
